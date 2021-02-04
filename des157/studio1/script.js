1ebb4b97be4845e25d068432e8dc218a0420dd11
Maxwell Langa <omlanga@ucdavis.edu>
2021-02-03 17:58:28 -0800

    home page created

--- des157/studio1/script.js
+++ des157/studio1/script.js
@@ -1,5 +1,38 @@
 (function(){
     "use strict"
     console.log("reading js");
-    
+    const myform= document.querySelector('#myform');
+    const poemlines = document.querySelector('#poemlines')
+
+    myform.addEventListener('submit', function(event){
+
+        document.getElementById('overlay').style.display = 'block'
+        event.preventDefault();
+        const loveName = document.querySelector('#loveName').value;
+        const bodyPart = document.querySelector('#bodyPart').value;
+        const verb = document.querySelector('#verb').value;
+        const family = document.querySelector('#family').value;
+        const userName = document.querySelector('#userName').value;
+
+        poemlines.innerHTML = `
+            <p>Roses are Red<p/>
+            <p>Violets are Blue<p/>
+            <p>Thinking of ${loveName}<p/>
+            <p>Made my ${bodyPart} BOOOO!<p/>
+            <p>But I ${verb} my ${family}'s Butt<p/>
+            <p>And They Yelled MOOOO!<p/>
+            <p id = "signature">Love ${userName}<p/>`
+
+    });
+
+    document.getElementById('newpoem').addEventListener("click", function(event){
+
+        document.getElementById('overlay').style.display = 'none'           
+        const userData = document.getElementsByClassName("info");
+        for( let data of userData){
+            data.value = "";
+        }
+        poemlines.innerHTML = "";
+
+    })
 })();
